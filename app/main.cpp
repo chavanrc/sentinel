11b4f52548ab685350e7e88f49fa99370c46a96f
rajendra <rajendra.alive@gmail.com>
2024-05-18 02:37:09 +0530

    working

--- app/main.cpp
+++ app/main.cpp
@@ -4,9 +4,9 @@
 int main() {
     sw::redis::SentinelOptions sentinel_opts;
     sentinel_opts.nodes = {
-            {"sentinel-1", 26379},
-            {"sentinel-2", 26379},
-            {"sentinel-3", 26379},
+            {"redis-sentinel-1", 26379},
+            {"redis-sentinel-2", 26379},
+            {"redis-sentinel-3", 26379},
     };
     sentinel_opts.connect_timeout = std::chrono::milliseconds(100);
     sentinel_opts.socket_timeout = std::chrono::milliseconds(100);
@@ -17,7 +17,7 @@ int main() {
     connection_opts.connect_timeout = std::chrono::milliseconds(100);
     connection_opts.socket_timeout = std::chrono::milliseconds(100);
     sw::redis::ConnectionPoolOptions pool_options;
-    std::string name = "mymaster";
+    std::string name = "redismaster";
     auto redis_master = std::make_shared<sw::redis::Redis>(redis_sentinel, name, sw::redis::Role::MASTER, connection_opts,
                                                            pool_options);
     auto redis_slave = std::make_shared<sw::redis::Redis>(redis_sentinel, name, sw::redis::Role::SLAVE,
